b9b69945f2757aa4f83300e260e95a484b47750a
Andreia Trianon <andreiatrianon@outlook.com>
2018-10-04 13:28:14 -0300

    1. get API pagina principal;2.criacao dos arquivos '.gitignore', 'config-firebase.js' e 'main.html'

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-var database = firebase.database();
-var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
+// var database = firebase.database();
+// var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
+let indexOfGif = 0;
+
+$('a').click(goToNextGif);
+
+function goToNextGif() {
+  
+}
+
+$.ajax({
+  type: 'GET',
+  url: `https://api.giphy.com/v1/gifs/search?q=gif&api_key=${keyAPI}&limit=100`,
+  success: carregarPosts,
+  error: erro
+});
+
+function carregarPosts(data) {
+  urlImg = data['data'][indexOfGif]['images']['original']['url'];
+  $('img').attr('src', urlImg);
+  // gifTitle = data['data'][indexOfGif]['title'];
+  // $('.gif-title').html(gifTitle);
+}
+
+function erro() {
+  console.log('erro');
+}
