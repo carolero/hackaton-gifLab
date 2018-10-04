21b266356242511991a5ad1b2a4698f49f2d4f90
Andreia Trianon <andreiatrianon@outlook.com>
2018-10-04 13:48:50 -0300

    proximo gif aparecendo depois de clicar nos botões

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 // var database = firebase.database();
 // var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
-let indexOfGif = 0;
-
-$('a').click(goToNextGif);
-
-function goToNextGif() {
-  
-}
-
-$.ajax({
-  type: 'GET',
-  url: `https://api.giphy.com/v1/gifs/search?q=gif&api_key=${keyAPI}&limit=100`,
-  success: carregarPosts,
-  error: erro
+$(document).ready(function () {
+  getGifsFromAPI();
+  $('a').click(getGifsFromAPI);
 });
 
-function carregarPosts(data) {
+let indexOfGif = -1;
+
+function getGifsFromAPI() {
+  $.ajax({
+    type: 'GET',
+    url: `https://api.giphy.com/v1/gifs/search?q=gif&api_key=${keyAPI}&limit=100`,
+    success: showGif,
+    error: erro
+  });
+}
+
+function showGif(data) {
+  indexOfGif += 1;
   urlImg = data['data'][indexOfGif]['images']['original']['url'];
   $('img').attr('src', urlImg);
   // gifTitle = data['data'][indexOfGif]['title'];
