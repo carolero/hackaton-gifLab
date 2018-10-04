180bd22e13a5975beb1374d9ee37aa1ea020190f
Georgete Bueno <geobueno@gmail.com>
2018-10-04 12:18:28 -0300

    1 commit Geo

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCI1rHWRJpSkxZFh8uv2ULJLF5wgbA4vso",
+    authDomain: "labgifs.firebaseapp.com",
+    databaseURL: "https://labgifs.firebaseio.com",
+    projectId: "labgifs",
+    storageBucket: "",
+    messagingSenderId: "465378932623"
+  };
+  firebase.initializeApp(config);
+
